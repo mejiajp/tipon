e3ec615a5331957824786d1a4c6438706f0ad2d3
mejia-1mt <mejiapaulo13@gmail.com>
2026-02-18 13:45:06 +0800

    added dto for user response

---
+++ backend/src/main/java/com/tipon/backend/auth/dto/UserResponse.java
@@ -0,0 +1,29 @@
+package com.tipon.backend.auth.dto;
+
+import com.tipon.backend.expense.Expense;
+import com.tipon.backend.user.AuthProvider;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+// Token + User info
+@Setter
+@Getter
+public class UserResponse {
+    private UUID id;
+    private String email;
+    private AuthProvider provider;
+    private LocalDateTime createdAt;
+    private List<Expense> expense;
+
+    public UserResponse(UUID id, String email, AuthProvider provider, LocalDateTime createdAt, List<Expense> expense) {
+        this.id = id;
+        this.email = email;
+        this.provider = provider;
+        this.createdAt = createdAt;
+        this.expense = expense;
+    }
+}
