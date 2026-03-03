7e0b88d4a0bb923e00383cccefd16d932272652e
mejia-1mt <mejiapaulo13@gmail.com>
2026-03-03 18:31:57 +0800

    adjusted uniform fetcher logic

--- frontend/lib/fetchers/expenses.ts
+++ frontend/lib/fetchers/expenses.ts
@@ -1,63 +1,56 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL + "/expenses";
 
-export async function getExpenses() {
-  try {
-    const res = await fetch(`${API_URL}`);
-    if (!res.ok) {
-      throw new Error("Failed to fetch expenses");
-    }
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching expenses:", error);
+export async function getAllExpenses() {
+  const res = await fetch(`${API_URL}`);
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Failed to fetch all expenses");
   }
+
+  return data;
 }
 
-export async function createExpense() {
-  try {
-    const res = await fetch(`${API_URL}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        amount: 123.45,
-        category: "Food",
-        description: "Test Lunch",
-      }),
-    });
+export async function createExpense(expense: {
+  amount: number;
+  category: string;
+  description: string;
+}) {
+  const res = await fetch(`${API_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(expense),
+  });
+  const data = await res.json();
 
-    if (!res.ok) {
-      throw new Error("Failed to add expense");
-    }
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("Error adding expense:", error);
+  if (!res.ok) {
+    throw new Error(data.message || "Failed to create expense");
   }
+
+  return data;
 }
 
 export async function getExpenseRange(start: string, end: string) {
-  try {
-    const res = await fetch(`${API_URL}?start=${start}&end=${end}`);
-    if (!res.ok) {
-      throw new Error("Failed to fetch expense range");
-    }
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching expense range:", error);
+  const res = await fetch(`${API_URL}?start=${start}&end=${end}`);
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(
+      data.message || "Failed to fetch expenses for the specified range"
+    );
   }
+
+  return data;
 }
 
 export async function getExpenseCalendar(year: string, month: string) {
   const res = await fetch(`${API_URL}/calendar?year=${year}&month=${month}`);
-
   const data = await res.json();
 
   if (!res.ok)
     throw new Error(data.message || "Failed to fetch expense calendar");
 
-  console.log(data);
   return data;
 }
