f101677069fe3d1f9edca3ed09103c3d44e545f0
mejia-1mt <mejiapaulo13@gmail.com>
2026-03-05 21:08:27 +0800

    added logic for login redirection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
-      <h1 className="text-3xl font-bold">Tipon</h1>
-    </div>
-  );
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const cookiesStore = await cookies();
+
+  const token = cookiesStore.get("token");
+
+  if (token) {
+    redirect("/dashboard");
+  }
+
+  redirect("/login");
 }
