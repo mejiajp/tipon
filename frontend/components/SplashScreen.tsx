c7da93dab0124c7eb61193a4e87e0a9f15e768db
mejia-1mt <mejiapaulo13@gmail.com>
2026-03-25 16:52:23 +0800

    SplashScreen initial design

---
+++ frontend/components/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SplashScreen() {
+  return (
+    // <main className="bg-black h-screen  flex justify-center items-center text-secondary text-title font-title">
+    //   <h1>tipon</h1>
+    // </main>
+    <main className="bg-primary h-screen  flex justify-center items-center text-text text-title font-title">
+      <h1>tipon</h1>
+    </main>
+  );
+}
