51551000c2ffc1a28e6a6ad9dc0f246f5639f49d
mejia-1mt <mejiapaulo13@gmail.com>
2026-03-26 14:44:01 +0800

    added svg for Navigation and color scheme for global css

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,13 +1,32 @@
 import Link from "next/link";
-import React from "react";
+import TransactionsIcon from "./icons/Transactions";
+import DashboardIcon from "./icons/Dashboard";
+import CalendarIcon from "./icons/Calendar";
+import NewIcon from "./icons/New";
+import ProfileIcon from "./icons/Profile";
 
 export default function Navigation() {
   return (
-    <div className="absolute bottom-0">
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/calendar">Calendar</Link>
-      <Link href="/transactions">Transactions</Link>
-      <Link href="/profile">Profile</Link>
-    </div>
+    <nav className="absolute bottom-0 flex items-center  h-15 w-full bg-white text-center  text-primary">
+      <Link href="/dashboard" className="h-12 flex-1">
+        <DashboardIcon className="h-[60%] w-full" />
+        <h3 className="text-smaller">Dashboard</h3>
+      </Link>
+      <Link href="/calendar" className="h-12 flex-1">
+        <CalendarIcon className="h-[60%] w-full" />
+        <h3 className="text-smaller">Calendar</h3>
+      </Link>
+      <Link href="/new" className="h-12 flex flex-1">
+        <NewIcon className="h-[60%] w-full  my-auto " />
+      </Link>
+      <Link href="/transactions" className="h-12 flex-1">
+        <TransactionsIcon className="h-[60%] w-full" />
+        <h3 className="text-smaller">Transactions</h3>
+      </Link>
+      <Link href="/profile" className="h-12 flex-1 ">
+        <ProfileIcon className="h-[60%] w-full" />
+        <h3 className="text-smaller">Profile</h3>
+      </Link>
+    </nav>
   );
 }
