44c00596f1d8d3342bbf29e84869fb716f2770bf
mejia-1mt <mejiapaulo13@gmail.com>
2026-03-27 17:13:57 +0800

    added icons for categories

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,31 +1,42 @@
 import Link from "next/link";
-import TransactionsIcon from "./icons/Transactions";
 import DashboardIcon from "./icons/Dashboard";
 import CalendarIcon from "./icons/Calendar";
 import NewIcon from "./icons/New";
+import TransactionIcon from "./icons/Transaction";
 import ProfileIcon from "./icons/Profile";
 
 export default function Navigation() {
   return (
-    <nav className="absolute bottom-0 flex items-center  h-15 w-full bg-white text-center  text-primary">
-      <Link href="/dashboard" className="h-12 flex-1">
-        <DashboardIcon className="h-[60%] w-full" />
-        <h3 className="text-smaller">Dashboard</h3>
+    <nav className=" h-[65px] bg-bg absolute bottom-5 left-1/2 -translate-x-1/2 flex rounded-[10px] p-[2px] gap-[2px]">
+      <Link
+        href="/dashboard"
+        className="w-15 aspect-square bg-bg-dark flex justify-center items-center rounded-[10px]"
+      >
+        <DashboardIcon className="w-6.5 h-6.5 text-text-muted" />
       </Link>
-      <Link href="/calendar" className="h-12 flex-1">
-        <CalendarIcon className="h-[60%] w-full" />
-        <h3 className="text-smaller">Calendar</h3>
+      <Link
+        href="/calendar"
+        className="w-15 aspect-square bg-bg-dark flex justify-center items-center rounded-[10px]"
+      >
+        <CalendarIcon className="w-6.5 h-6.5 text-text-muted" />
       </Link>
-      <Link href="/new" className="h-12 flex flex-1">
-        <NewIcon className="h-[60%] w-full  my-auto " />
+      <Link
+        href="/new"
+        className="w-15 aspect-square bg-primary flex justify-center items-center rounded-[10px]"
+      >
+        <NewIcon className="w-6.5 h-6.5 text-bg-dark" />
       </Link>
-      <Link href="/transactions" className="h-12 flex-1">
-        <TransactionsIcon className="h-[60%] w-full" />
-        <h3 className="text-smaller">Transactions</h3>
+      <Link
+        href="/transaction"
+        className="w-15 aspect-square bg-bg-dark flex justify-center items-center rounded-[10px]"
+      >
+        <TransactionIcon className="w-6.5 h-6.5 text-text-muted" />
       </Link>
-      <Link href="/profile" className="h-12 flex-1 ">
-        <ProfileIcon className="h-[60%] w-full" />
-        <h3 className="text-smaller">Profile</h3>
+      <Link
+        href="/profile"
+        className="w-15 aspect-square bg-bg-dark flex justify-center items-center rounded-[10px]"
+      >
+        <ProfileIcon className="w-6.5 h-6.5 text-text-muted" />
       </Link>
     </nav>
   );
