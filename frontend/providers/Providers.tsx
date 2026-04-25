493e1f2959c0a97206e27253980d077beff68c58
mejia-1mt <mejiapaulo13@gmail.com>
2026-04-25 22:21:50 +0800

    initialize googleauth setup

--- frontend/providers/Providers.tsx
+++ frontend/providers/Providers.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import AuthProvider from "./AuthProvider";
 import ThemeProvider from "./ThemeProvider";
 import ToastProvider from "./ToastProvider";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        {children}
-        <ToastProvider />
-      </ThemeProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <AuthProvider>
+        <ThemeProvider>
+          {children}
+          <ToastProvider />
+        </ThemeProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
