4320c81d69e5f4355b0a54efdc662713e33e96d2
mejia-1mt <mejiapaulo13@gmail.com>
2026-05-01 21:38:28 +0800

    google button frontend

--- frontend/app/(auth)/login/GoogleButton.tsx
+++ frontend/app/(auth)/login/GoogleButton.tsx
@@ -2,6 +2,8 @@
 
 import { googleLogin } from "@/lib/api/users.client";
 import { useGoogleLogin } from "@react-oauth/google";
+import GoogleIcon from "@/public/images/google-icon.png";
+import Image from "next/image";
 
 export default function GoogleButton() {
   const login = useGoogleLogin({
@@ -16,8 +18,19 @@ export default function GoogleButton() {
   });
 
   return (
-    <button onClick={() => login()} className="full-button outline">
-      Continue with Google
+    <button
+      onClick={() => login()}
+      className="full-button outline flex justify-center items-center"
+    >
+      <Image
+        width="100"
+        height="100"
+        src={GoogleIcon.src}
+        alt="Google Icon"
+        className="w-5 h-5 mr-2"
+      />
+
+      <span> Continue with Google</span>
     </button>
   );
 }
