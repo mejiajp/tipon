eb0283551e90e3b2e1aab4f2f9a11e1225238662
mejia-1mt <mejiapaulo13@gmail.com>
2026-05-20 23:43:18 +0800

    incorporate google login in the login flow

--- frontend/app/(auth)/login/GoogleButton.tsx
+++ frontend/app/(auth)/login/GoogleButton.tsx
@@ -5,23 +5,37 @@ import { useGoogleLogin } from "@react-oauth/google";
 import GoogleIcon from "@/public/images/google-icon.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useToastStore } from "@/stores/useToastStore";
 
 export default function GoogleButton() {
   const router = useRouter();
 
+  const refreshAuth = useAuthStore((state) => state.refreshAuth);
+  const addToast = useToastStore((state) => state.addToast);
+
   const login = useGoogleLogin({
     flow: "auth-code",
+
     onSuccess: async (res) => {
-      console.log(res);
       try {
         await googleLogin(res.code);
+
+        // IMPORTANT
+        await refreshAuth();
+
+        addToast(`Welcome ${useAuthStore.getState().user?.name}!`, "success");
+
         router.replace("/home");
       } catch (err) {
         console.log(err);
+        addToast("Google login failed", "error");
       }
     },
+
     onError: (error) => {
       console.log(error);
+      addToast("Google login failed", "error");
     },
   });
 
@@ -38,7 +52,7 @@ export default function GoogleButton() {
         className="w-5 h-5 mr-2"
       />
 
-      <span> Continue with Google</span>
+      <span>Continue with Google</span>
     </button>
   );
 }
