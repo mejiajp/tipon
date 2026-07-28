af5de3e4d0067a11bfb71fc9c7eead6d02b5864e
mejia-1mt <mejiapaulo13@gmail.com>
2026-07-28 23:41:13 +0800

    logo placeholder

--- frontend/components/SplashScreen.tsx
+++ frontend/components/SplashScreen.tsx
@@ -1,7 +1,7 @@
 export default function SplashScreen() {
   return (
     <main className="bg-bg-dark h-screen  flex justify-center items-center text-primary text-title font-title">
-      <h1>t</h1>
+      <h1>T</h1>
     </main>
   );
 }
