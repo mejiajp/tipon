4c6646611416cb3661ec635b8d9e0bdaf77ac630
mejia-1mt <mejiapaulo13@gmail.com>
2026-09-25 22:59:40 +0800

    rfc template

--- frontend/components/SplashScreen.tsx
+++ frontend/components/SplashScreen.tsx
@@ -1,11 +1,7 @@
-import React from "react";
-
-function SplashScreen() {
+export default function SplashScreen() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-5xl text-primary font-title ">tipon</h1>
     </main>
   );
 }
-
-export default SplashScreen;
